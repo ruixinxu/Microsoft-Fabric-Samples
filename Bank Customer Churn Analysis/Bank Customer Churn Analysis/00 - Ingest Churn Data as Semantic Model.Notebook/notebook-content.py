6cfd170f3e1b2e15dd3facfea327af3a316dc428
Ruixin Xu <ruixinxu@users.noreply.github.com>
2026-02-06 15:21:47 +0800

    Committing 2 items from workspace f5d4f720-7eb0-4cc1-b70d-e1d912197072

--- Bank Customer Churn Analysis/Bank Customer Churn Analysis/00 - Ingest Churn Data as Semantic Model.Notebook/notebook-content.py
+++ Bank Customer Churn Analysis/Bank Customer Churn Analysis/00 - Ingest Churn Data as Semantic Model.Notebook/notebook-content.py
@@ -20,6 +20,10 @@
 # META   }
 # META }
 
+# MARKDOWN ********************
+
+# # Load the Churn users
+
 # CELL ********************
 
 df_raw = spark.read.format("csv").option("header","true").load("abfss://f5d4f720-7eb0-4cc1-b70d-e1d912197072@onelake.dfs.fabric.microsoft.com/d9e46ba2-db50-43b6-b3c0-a95889986de7/Files/churn.csv")
@@ -73,6 +77,11 @@ df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").sav
 # META   "language_group": "synapse_pyspark"
 # META }
 
+# MARKDOWN ********************
+
+# ## Create a semantic model and add the churn table into the semantic model
+
+
 # CELL ********************
 
 # Create a semantic model and add the churn table into the semantic model
@@ -85,6 +94,63 @@ plt.axis('off')
 # Plot image in the output
 image_plot = plt.imshow(image)
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# # Load the Active Users
+
+
+# CELL ********************
+
+df_active = spark.read.format("csv").option("header","true").load("abfss://f5d4f720-7eb0-4cc1-b70d-e1d912197072@onelake.dfs.fabric.microsoft.com/d9e46ba2-db50-43b6-b3c0-a95889986de7/Files/active.csv")
+# df now is a Spark DataFrame containing CSV data from "abfss://f5d4f720-7eb0-4cc1-b70d-e1d912197072@onelake.dfs.fabric.microsoft.com/d9e46ba2-db50-43b6-b3c0-a95889986de7/Files/churn.csv".
+display(df_active)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import col
+
+df_active_clean = (
+    df_active
+    .withColumn("RowNumber", col("RowNumber").cast("int"))
+    .withColumn("CustomerId", col("CustomerId").cast("long"))
+    .withColumn("CreditScore", col("CreditScore").cast("int"))
+    .withColumn("Age", col("Age").cast("int"))
+    .withColumn("Tenure", col("Tenure").cast("int"))
+    .withColumn("Balance", col("Balance").cast("double"))
+    .withColumn("NumOfProducts", col("NumOfProducts").cast("int"))
+    .withColumn("HasCrCard", col("HasCrCard").cast("int"))
+    .withColumn("IsActiveMember", col("IsActiveMember").cast("int"))
+    .withColumn("EstimatedSalary", col("EstimatedSalary").cast("double"))
+)
+
+display(df_active_clean)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+delta_table_path = "Tables/active" #fill in your delta table path 
+df_active_clean.write.format("delta").mode("overwrite").option("overwriteSchema", "true").save(delta_table_path)
 
 # METADATA ********************
 
